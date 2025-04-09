6baf549cb37c84fb0163376972bcfcde14d9336c
harrison_clarke <harrisonclarke@kubrickgroup.com>
2025-04-09 15:25:15 +0100

    added authenticate function

--- authenticate.py
+++ authenticate.py
@@ -1,6 +1,7 @@
 """This is the basic authenticator for the app"""
 
 
-def authenticate(username, password):
+def authenticate(username: str, password: str) -> bool:
     """This function will check that the username
     and the password are an exact match"""
+    return False
