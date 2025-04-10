39f5275d07a27ea28738c2a8234f30a030cd5465
harrison_clarke <harrisonclarke@kubrickgroup.com>
2025-04-10 15:09:44 +0100

    changes in format

--- query_db.py
+++ query_db.py
@@ -1,5 +1,13 @@
-"""This module retrieves information from the database and returns a list of user specific exercises in the form of their UserExerciseID"""
+"""This module retrieves information from the database and
+returns a list of user specific exercises in the form of their UserExerciseID"""
 
 import sqlite3
 
-conn = sqlite3.Connection(None)
+
+def connect(db_path: str = "") -> sqlite3.Connection:
+    """This function connects to the database
+     Input:
+        db_path(str): The pathway to the database
+    Returns:
+        sqlite3.Connection"""
+    return sqlite3.Connection(db_path)
