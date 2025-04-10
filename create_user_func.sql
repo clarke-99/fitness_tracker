d5ef7657d78fba46a9f7fcb74252fb0667a7d1d8
harrison_clarke <harrisonclarke@kubrickgroup.com>
2025-04-10 18:25:45 +0100

    forgot to save create user

--- create_user_func.sql
+++ create_user_func.sql
@@ -30,3 +30,51 @@ BEGIN
     RETURN new_user_id;
 END;
 $$ LANGUAGE plpgsql;
+
+
+SELECT create_new_user(
+    'TestFirst'::VARCHAR,
+    'TestLast'::VARCHAR,
+    TRUE,
+    'testuser@example.com'::VARCHAR,
+    '1995-06-15'::DATE,
+    'testuser123'::VARCHAR,
+    'hashedpassword123'::VARCHAR,
+    180::SMALLINT,
+    75::SMALLINT,
+    '2025-04-10'::DATE
+);
+RETURNS INT AS $$
+DECLARE
+    new_user_id INT;
+BEGIN
+    -- Insert into dimUser
+    INSERT INTO dimUser (FirstName, LastName, SexMale, Email, DOB)
+    VALUES (f_name, l_name, sex, email, dob)
+    RETURNING UserID INTO new_user_id;
+
+    -- Insert into authUser
+    INSERT INTO authUser (UserName, Password, UserID)
+    VALUES (username, password_hash, new_user_id);
+
+    -- Insert into factUserMetrics
+    INSERT INTO factUserMetrics (UserID, HeightCm, WeightKg, DateRecorded)
+    VALUES (new_user_id, height_cm, weight_kg, date_recorded);
+
+    RETURN new_user_id;
+END;
+$$ LANGUAGE plpgsql;
+
+-- example how to use
+-- SELECT create_new_user(
+--     'TestFirst'::VARCHAR,
+--     'TestLast'::VARCHAR,
+--     TRUE,
+--     'testuser@example.com'::VARCHAR,
+--     '1995-06-15'::DATE,
+--     'testuser123'::VARCHAR,
+--     'hashedpassword123'::VARCHAR,
+--     180::SMALLINT,
+--     75::SMALLINT,
+--     '2025-04-10'::DATE
+-- );
